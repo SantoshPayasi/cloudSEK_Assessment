057b4b2a449953e5d027b238c21ae8816aedcb3c
SantoshPayasi <santoshpayasi78@gmail.com>
2024-11-18 06:08:37 +0530

    changes push

---
+++ backend/src/repository/comments.repository.js
@@ -0,0 +1,32 @@
+import commentsModel from "../models/comments.model.js"
+
+class CommentRepository{
+    static async create(data){
+        console.log(data)
+        const comment = await new commentsModel(data).save()
+        return comment;
+    }
+
+    static async delete(filter){
+        console.log(filter, "This is")
+        const comment = await commentsModel.findOneAndDelete(filter)
+        console.log("Comment is repo", comment)
+        return comment;
+    }
+
+
+    static async update(filter, data){
+        const comment = await commentsModel.findOneAndUpdate(filter, data, {new: true})
+        return comment;
+    }
+
+    static async deleteMany(filter, options ={}){
+        const deletedComments = await commentsModel.deleteMany(filter, options)
+        return deletedComments;
+    }
+}
+
+
+
+
+export default CommentRepository
